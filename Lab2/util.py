1dcac3bd0ccfb1f7b0dc0c15be23b6522a1392a3
Eric Smith <eric@smu.edu>
2019-03-07 17:08:06 -0600

    able to train encoders and decoders

--- Lab2/util.py
+++ Lab2/util.py
@@ -2,7 +2,32 @@ import multiprocessing.pool
 from functools import partial
 import os
 
-from keras.preprocessing.image import _count_valid_files_in_directory
+# from keras.preprocessing.image import _count_valid_files_in_directory
+
+def _count_valid_files_in_directory(directory, white_list_formats, follow_links):
+    """Count files with extension in `white_list_formats` contained in a directory.
+    # Arguments
+        directory: absolute path to the directory containing files to be counted
+        white_list_formats: set of strings containing allowed extensions for
+            the files to be counted.
+    # Returns
+        the count of files with extension in `white_list_formats` contained in
+        the directory.
+    """
+    def _recursive_list(subpath):
+        return sorted(os.walk(subpath, followlinks=follow_links), key=lambda tpl: tpl[0])
+
+    samples = 0
+    for root, _, files in _recursive_list(directory):
+        for fname in files:
+            is_valid = False
+            for extension in white_list_formats:
+                if fname.lower().endswith('.' + extension):
+                    is_valid = True
+                    break
+            if is_valid:
+                samples += 1
+    return samples
 
 def count_num_samples(directory):
     """
