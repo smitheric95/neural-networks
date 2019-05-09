ec1c959d73cc495a9666ea461ef6a6f18b952f06
Eric Smith <eric@smu.edu>
2019-05-09 15:16:01 -0500

    added paper code

---
+++ Paper/DQN_Agent.py
@@ -0,0 +1,106 @@
+#reference: https://keon.io/deep-q-learning/
+# this dqn is used as a baseline and starter code for our improved DQN
+import numpy as np
+import random
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import Adam
+from Agent import Agent
+
+class DQN_Agent(Agent):
+    def __init__(self, env):
+        self.env = env
+        self.replay_memory = deque(maxlen=200000)
+
+        self.gamma = 0.99
+        self.epsilon = 1.0
+        self.epsilon_min = 0.01
+        self.epsilon_decay = 0.9995
+        self.learning_rate = 0.05
+        self.target_update_counter = 0
+        self.C = 8 # intervcal for updating target network
+        self.initial_random_steps = 0
+        self.actions_count = 0
+        self.clip_errors = True
+
+        self.q_network = self.init_q_network()
+        self.target_q_network = self.init_q_network()
+        self.name = "DQN"
+
+    def on_episode_start(self):
+        pass
+
+    def on_episode_end(self):
+        pass
+
+    def get_observation_space(self):
+        return self.env.observation_space
+
+    def get_action_space(self):
+        return self.env.action_space
+
+    def init_q_network(self):
+        model = Sequential()
+        state_shape = self.get_observation_space().shape
+        model.add(Dense(48, input_shape=state_shape, activation="relu"))
+        #model.add(Dense(48, activation="relu"))
+        #model.add(Dense(24, activation="relu"))
+        model.add(Dense(self.get_action_space().n, activation='linear'))
+        model.compile(loss="mean_squared_error", optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def act(self, state):
+        self.actions_count += 1
+        self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon_min, self.epsilon)
+        if np.random.random() < self.epsilon or self.actions_count < self.initial_random_steps:
+            return self.get_action_space().sample()
+        return np.argmax(self.q_network.predict(state)[0])
+
+    def update_model(self, state, action, reward, new_state, done):
+        self.replay_memory.append([state, action, reward, new_state, done])
+        self.fit_q_network()
+        self.update_target_q_network()
+
+    def sample_replays(self,batch_size):
+        return random.sample(self.replay_memory, batch_size)
+
+    def fit_q_network(self):
+        #sample replay and do SGD
+        batch_size = 16
+        if len(self.replay_memory) < batch_size:
+            return
+
+        samples = self.sample_replays(batch_size)
+        sampled_states = []
+        sampled_targets = []
+        for sample in samples:
+            state, action, reward, new_state, done = sample
+            target = self.target_q_network.predict(state)
+            predicted = self.q_network.predict(state)
+            if done:
+                target[0][action] = reward
+            else:
+                #update target by Bellman equation
+                target[0][action] = reward + self.gamma * max(self.target_q_network.predict(new_state)[0])
+
+                if self.clip_errors:
+                    #clip error to -1, +1
+                    if (target[0][action] > predicted[0][action]):
+                        target[0][action] = predicted[0][action] + 1
+                    elif (target[0][action] > predicted[0][action]):
+                        target[0][action] = predicted[0][action] - 1
+            sampled_states.append(state)
+            sampled_targets.append(target)
+
+        batched_states = np.concatenate(sampled_states,axis=0)
+        batched_targets = np.concatenate(sampled_targets,axis=0)
+        self.q_network.fit(batched_states, batched_targets, epochs=1, verbose=0)
+
+    def update_target_q_network(self):
+        #update target q network every C steps
+        self.target_update_counter += 1
+        if (self.target_update_counter > self.C):
+            self.target_update_counter = 0
+            self.target_q_network.set_weights(self.q_network.get_weights())
---
+++ Paper/Helpers.py
@@ -0,0 +1,107 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import random
+import pandas as pd
+
+def make_multi_env(scenario_name, benchmark=False,done_cb=None):
+    from multiagent.environment import MultiAgentEnv
+    import multiagent.scenarios as scenarios
+
+    # load scenario from script
+    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    # create world
+    world = scenario.make_world()
+    # create multiagent environment
+    if benchmark:
+        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data,done_callback=done_cb)
+    else:
+        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation,done_callback=done_cb)
+    return env
+
+def plot_state_scatter(agent,title1,title2,xlabel1,ylabel1,xlabel2,ylabel2,color, lim1 = [-0.1,0.1,-1.4,0.6],lim2=[-2.0,1.0,-2.0,2.0]):
+    fig = plt.figure()
+
+    a = []
+    b = []
+    sample_size = min(2000,len(agent.replay_memory))
+    for sample in random.sample(agent.replay_memory, sample_size):
+        a.append(sample[0][0][0])
+        b.append(sample[0][0][1])
+
+    sub1 = fig.add_subplot(2,2,1)
+    sub1.grid(True,linewidth='0.4',color='white')
+    sub1.set_xlabel(xlabel1)
+    sub1.set_ylabel(ylabel1)
+    sub1.set_ylim(bottom=lim1[0],top = lim1[1])
+    sub1.yaxis.set_major_locator(ticker.MultipleLocator(0.02))
+    sub1.set_xlim(left=lim1[2],right=lim1[3])
+    sub1.xaxis.set_major_locator(ticker.MultipleLocator(0.25))
+    sub1.set_facecolor('#e6f3ff')
+    sub1.scatter(a,b,s=3,color = color)
+
+    if len(sample[0][0]) <= 2:
+        return
+    c = []
+    d = []
+    for sample in random.sample(agent.replay_memory, sample_size):
+        c.append(sample[0][0][2])
+        d.append(sample[0][0][3])
+
+    sub2 = fig.add_subplot(2,2,2)
+    sub2.grid(True,linewidth='0.4',color='white')
+    sub2.set_xlabel(xlabel2)
+    sub2.set_ylabel(ylabel2)
+    sub2.set_ylim(bottom=lim2[0],top = lim2[1])
+    sub2.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
+    sub2.set_xlim(left=lim2[2],right=lim2[3])
+    sub2.xaxis.set_major_locator(ticker.MultipleLocator(0.5))
+    sub2.set_facecolor('#e6f3ff')
+    sub2.scatter(c,d,s=3,color = color)
+
+    sub2.savefig('Plots/%s_state_scatter.png'%title)
+
+def plot_rewards_and_length(rewards, min_reward,max_reward, lengths):
+
+    rewards_df = pd.DataFrame(rewards)
+    rewards_df.to_csv('Data/rewards.csv')
+
+    fig = plt.figure()
+    sub1 = fig.add_subplot(2,2,1)
+    sub1.set_title('Reward')
+    sub1.set_ylim(bottom=min_reward,top=max_reward)
+    sub1.set_xlabel('episodes')
+    sub1.set_ylabel('reward')
+    sub1.plot(rewards)
+
+    '''
+    sub2 = fig.add_subplot(2,2,2)
+    sub2.set_title('episode length')
+    sub2.set_xlabel('episodes')
+    sub2.plot(lengths)
+    '''
+
+
+    avg_reward = [0.] * len(rewards)
+    cumulative_rewards = [0.] * len(rewards)
+    cumulated_r = 0.
+    for i in range(len(rewards)):
+        cumulated_r += rewards[i]
+        cumulative_rewards[i] = cumulated_r
+    #interval = 10
+
+    for i in range(len(rewards)):
+        if i <= 0:
+            avg_reward[i] = rewards[i]
+        else:
+            avg_reward[i] = (cumulative_rewards[i] - cumulative_rewards[0])/i
+    sub3 = fig.add_subplot(2,2,2)
+    sub3.set_ylim(bottom=min_reward,top=max_reward)
+    sub3.set_title('average rewards')
+    sub3.set_xlabel('episodes')
+    sub3.plot(avg_reward)
+    
+    # plt.show()
+    sub2.savefig('Plots/%s_rewards.png'%title)
+
+
+
